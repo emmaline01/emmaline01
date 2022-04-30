40e39524ca1d8260928f8b6be3f2020f8d4f4109
emmaline01 <emmaline.mai@gmail.com>
2022-04-30 12:19:59 -0400

    added modals for project descriptions

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// referenced https://www.w3schools.com/howto/howto_css_modals.asp
+
+var modals = ["biorobotics", "cardistry", "elm", "ukulelebot"]
+
+// allow modal to appear/disappear
+for (let i = 0; i < modals.length; i++) {
+    let modal = document.getElementById(modals[i] + "-modal");
+    let btn = document.getElementById(modals[i] + "-button");
+    let span = document.getElementsByClassName(modals[i] + "-close")[0];
+
+    // open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // close the modal on x click
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+// close all modals on outside click
+window.onclick = function(event) {
+    for (let i = 0; i < modals.length; i++) {
+        let modal = document.getElementById(modals[i] + "-modal");
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
