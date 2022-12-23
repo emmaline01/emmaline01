cf2202db26c1e0637194257d0bf0b3065c89461e
Emmaline Mai <emmaline.mai@gmail.com>
2022-12-22 16:33:04 -0800

    rethemed the website

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // referenced https://www.w3schools.com/howto/howto_css_modals.asp
 
-var modals = ["cheerpeer", "biorobotics", "cardistry", "elm", "ukulelebot", "dailyDino"]
+var modals = ["cheerpeer", "biorobotics", "cardistry", "elm", "dailyDino"]
 
 // allow modal to appear/disappear
 for (let i = 0; i < modals.length; i++) {
@@ -9,18 +9,18 @@ for (let i = 0; i < modals.length; i++) {
     let span = document.getElementsByClassName(modals[i] + "-close")[0];
 
     // open the modal
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal.style.display = "block";
     }
 
     // close the modal on x click
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
 }
 
 // close all modals on outside click
-window.onclick = function(event) {
+window.onclick = function (event) {
     for (let i = 0; i < modals.length; i++) {
         let modal = document.getElementById(modals[i] + "-modal");
         if (event.target == modal) {
@@ -31,10 +31,10 @@ window.onclick = function(event) {
 
 // check that screen isn't too small
 // https://stackoverflow.com/questions/48719745/displaying-an-alert-for-all-mobile-devices
-window.onload=function() {
+window.onload = function () {
     var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
     if (mobile) {
-        alert("Visit this website on a computer for a better view!");              
+        alert("Visit this website on a computer for a better view!");
     }
-    
+
 }
